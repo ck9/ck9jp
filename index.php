651718dbda62a5e88de9315fd715362271bb045d
ck9jp <0926cooie@gmail.com>
2019-10-19 01:49:08 +0900

    master

---
+++ index.php
@@ -0,0 +1,157 @@
+<html>
+
+<head>
+
+  <meta name=viewport content="width=device-width, initial-scale=1">
+  <meta charset="UTF-8">
+
+  <title>ck9.jp</title>
+  <meta name="description" content="@0926cookie">
+  <link rel="shortcut icon" href="favicon.ico">
+
+  <meta property="og:title" content="ck9.jp">
+  <meta property="og:url" content="https://ck9.jp/">
+  <meta property="og:image" content="./image/cat.jpg" />
+  <meta property="og:description" content="@0926cookie">
+  
+  <link href="//cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+
+</head>
+
+<body> 
+
+    <nav>
+      <div class="nav-wrapper blue-grey darken-3">
+        <a href="https://ck9.jp" class="brand-logo center">ck9.jp</a>
+      </div>
+    </nav>
+
+<div class="row">
+
+ <div class="col s12 m6">
+  <div class="card blue-grey white">
+  <div class="card-content black-text">
+
+  <span class="card-title">About me</span>
+
+   <ul class="collection with-header">
+     <li class="collection-item">Handle: くっきー, cookie, ck9</li>
+     <li class="collection-item">Birth: 2000/09/26</li>
+     <li class="collection-item"><script>(function(d){var s='61646d696e40636b392e6a70';var m='';for(var i=0;i<s.length;i+=2){m+=String.fromCharCode('0x'+s[i]+s[i+1]);}d.write('Email: '+m);})(document);</script></li>
+     <li class="collection-item"> Programming: C++, PHP</li>
+   </ul>
+
+  <span class="card-title">Member of</span>
+    
+   <ul class="collection with-header">
+     <li class="collection-item"><a href="https://apcc.jp/">Azabu Personal Computer Club</a> (31st OB)</li>
+     <!--
+     <li class="collection-item"></li>
+     <li class="collection-item"></li>
+     -->
+   </ul>
+
+  </div>
+  </div>
+ </div>
+
+
+ <div class="col s12 m6">
+  <div class="card blue-grey white">
+  <div class="card-content black-text">
+
+  <span class="card-title">Links / Products</span>
+
+   <ul class="collection">
+
+    <a href="https://twitter.com/cookie_imcg" class="collection-item avatar">
+      <img src="./image/icon/Twitter.png" class="circle"　>
+      <span class="title">Twitter</span>
+      <p>@cookie_imcg</p>
+    </a>
+
+    <a href="https://blog.ck9.jp/" class="collection-item avatar" >
+      <img src="./image/icon/WordPress.png" class="circle">
+      <span class="title">Blog</span>
+      <p>くっきーの備忘録</p>
+    </a>
+
+    <a href="https://github.com/ck9jp" class="collection-item avatar" >
+      <img src="./image/icon/GitHub.png" class="circle"　>
+      <span class="title">GitHub</span>
+      <p>ck9jp</p>
+    </a>
+    
+    <a href="https://imcg.pink-check.school/producer/detail/139009876" class="collection-item avatar">
+      <img src="./image/icon/imas_cg.png" class="circle">
+      <span class="title">imcg(ID:139009876)</span>
+      <p>アイドルマスター シンデレラガールズ </p>
+    </a>
+
+    <a href="http://line.me/S/sticker/1091790" class="collection-item avatar" >
+      <img src="./image/icon/LINE.png" class="circle"　>
+      <span class="title">LINE Stickers</span>
+      <p>くっきーのねことそのなかまたち</p>
+    </a>
+
+    <a href="https://zeny.ck9.jp" class="collection-item avatar" >
+      <img src="./image/icon/bitzeny.png" class="circle"　>
+      <span class="title">JPY/Bitzeny chart</span>
+      <p>JPY/ZNYを算出＆記録</p>
+    </a>
+
+    <a href="https://ck9.jp/LINE/" class="collection-item avatar" >
+      <img src="./image/icon/cat.png" class="circle"　>
+      <span class="title">LINE Stickers Image Viewer</span>
+      <p>スタンプIDを入力すると該当LINEスタンプを画像形式で一覧表示</p>
+    </a>
+
+   </ul>
+
+  </div>
+  </div>
+ </div>
+
+
+<!-- ブログ最新記事表示
+
+ <div class="col s12 m6">
+  <div class="card blue-grey white">
+  <div class="card-content black-text">
+  <span class="card-title">Blog</span>
+
+  <div class="collection">
+<?/*
+$rss = simplexml_load_file("https://blog.ck9.jp/feed");
+$rss = json_decode(json_encode($rss), true);
+
+foreach ($rss["channel"]["item"] as $key => $value) {
+  echo '<a href="'.$value["link"].'" class="collection-item">'.$value["title"].'</a>';
+}
+*/?>
+  </div>
+
+  </div>
+  </div>
+ </div>
+
+-->
+
+</div>
+
+<?
+$copyright_text="©︎ 2017-".date("Y")." ck9.jp";
+?>
+
+  <div class="footer">
+    <div class="container">
+     <div class="row center">
+       <p><?=$copyright_text?></p>
+     </div>
+    </div>
+  </div>
+
+
+</body>
+
+</html>
